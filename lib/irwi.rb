c52726f5ab8a6f95136f5c88705d9a21f4ca3dab
Alexey Noskov <alexey.noskov@gmail.com>
2009-11-25 00:52:58 +0300

    Submodule decalrations

--- lib/irwi.rb
+++ lib/irwi.rb
@@ -1,4 +1,11 @@
 module Irwi
+
+  module Formatters; end
+  module Comparators; end
+  module Extensions; end
+  module Helpers; end
+  module Paginators; end
+  module Support; end
   
   def self.config
     require 'irwi/config'
