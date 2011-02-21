bea7f3c17c04fb79ad24beba6df0d75f591599cb
Craig Ulliott <craig@whereivebeen.com>
2011-02-20 21:12:11 -0800

    after attaching a file, redirect back to the correct wiki page

--- lib/irwi/extensions/controllers/wiki_page_attachments.rb
+++ lib/irwi/extensions/controllers/wiki_page_attachments.rb
@@ -3,7 +3,7 @@ module Irwi::Extensions::Controllers::WikiPageAttachments
   def add_attachment
     attachment = page_attachment_class.new(params[:wiki_page_attachment])
     attachment.save
-    redirect_to url_for( :action => :edit)
+    redirect_to url_for(:path => attachment.page.path, :action => :edit)
   end
 
   def remove_attachment
