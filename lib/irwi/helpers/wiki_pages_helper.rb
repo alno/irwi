0613686f9b83d048ffe5a6c71bad9bfcea304fd7
Alexey Noskov <alexey.noskov@gmail.com>
2016-04-17 20:27:52 +0300

    Extract wiki_page_attachments to partial

--- lib/irwi/helpers/wiki_pages_helper.rb
+++ lib/irwi/helpers/wiki_pages_helper.rb
@@ -124,21 +124,6 @@ module Irwi::Helpers::WikiPagesHelper
   end
 
   def wiki_page_attachments(page = @page)
-    return unless Irwi.config.page_attachment_class_name
-
-    page.attachments.each do |attachment|
-      concat image_tag(attachment.wiki_page_attachment.url(:thumb))
-      concat "Attachment_#{attachment.id}"
-      concat link_to(wt('Remove'), wiki_remove_page_attachment_path(attachment.id), method: :delete)
-    end
-
-    form_for(Irwi.config.page_attachment_class.new,
-             as: :wiki_page_attachment,
-             url: wiki_add_page_attachment_path(page),
-             html: { multipart: true }) do |form|
-      concat form.file_field :wiki_page_attachment
-      concat form.hidden_field :page_id, value: page.id
-      concat form.submit 'Add Attachment'
-    end
+    render partial: "#{template_dir '_wiki_page_actions'}/wiki_page_attachments", locals: { page: page }
   end
 end
